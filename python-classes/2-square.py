05b972235f0e8377e5059d498e8d2a43a337633c
abdel <chaffylace@gmail.com>
2026-01-28 13:23:38 +0100

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class.
-It demonstrates data validation using property setters and getters logic.
-"""
+""" class Square """
+
 
 class Square:
-    """
-    This class defines a square by its size and validates it.
-    """
+    """ square class """
+
     def __init__(self, size=0):
-        """
-        Initializes a new Square instance.
-
-        Args:
-            size (int): The size of the square. Defaults to 0.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
+        """ initialise size of square """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if isinstance(size, int) and size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
-        
