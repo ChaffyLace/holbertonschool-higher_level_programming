e80c2897a205bd96c474cde79230fbace62713b7
abdel <chaffylace@gmail.com>
2026-01-30 09:39:41 +0100

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Rectangle class."""
 
 class Rectangle:
-    """ Defines a rectangle """
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -21,6 +24,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
