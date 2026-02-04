772c6a7f8ea92900830317b3b2a995ad6413030e
abdel <abdelmouridelmrabit07@gmail.com>
2026-02-04 22:43:53 +0100

    pycdoestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """subclass inheritance."""
 
+
 def inherits_from(obj, a_class):
-    """Check if the object is an instance of a class that inherited from a_class."""
+    """Check if the object is an instance of a 
+    class that inherited from a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
