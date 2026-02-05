2781141733b6715374091ccb0f12857c1cab8116
abdel <chaffylace@gmail.com>
2026-02-05 15:05:07 +0100

    task 3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module task_03_countediterator."""
+
+
+class CountedIterator:
+    """Class to iterate over a sequence and count items."""
+
+    def __init__(self, data):
+        """Initialize the iterator from data and set counter to zero."""
+        self.iterator = iter(data)
+        self.counter = 0
+
+    def get_count(self):
+        """Return the current value of the counter."""
+        return self.counter
+
+    def __next__(self):
+        """Fetch next item, increment counter and return item."""
+        item = next(self.iterator)
+        self.counter += 1
+        return item
+
+    def __iter__(self):
+        """Return the iterator object itself to allow loop usage."""
+        return self
