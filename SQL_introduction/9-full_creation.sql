f71a2b075b9a6dd54b7370656c4857d9f8ecef00
abdel <abdelmouridelmrabit07@gmail.com>
2026-03-05 11:48:44 +0000

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,8 @@
---create 
-CREATE TABLE second_table IF NOT EXIST;
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8);
+-- Creates a table and inserts multiple rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
