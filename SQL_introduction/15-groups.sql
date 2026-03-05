9bfdd6204669f0e540628db535cbefc1a29596f1
abdel <abdelmouridelmrabit07@gmail.com>
2026-03-05 14:38:29 +0000

    task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
---list the number
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY  number DESC;
+-- Lists the number
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
